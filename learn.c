9dd01e9c8ef3531da6baf8b373050ca6bfd8f865
Huaping.Huang <huaping.huang@eastech.com>
2017-11-02 13:49:12 +0800

    flag2

--- learn.c
+++ learn.c
@@ -7,6 +7,7 @@ void Learn_IR_Key_Process(void)
 	   	g_fgIrNewKey_multi_ir = 0;
 	      if (g_ucKeyCode==_KEY_NOKEY)
           	{
+          	playkeyy=keynokey;
 	   		g_ucKeyCode=(System_Key_Type)IR_key_convert_Multi_Ir();
           	}
    	}
